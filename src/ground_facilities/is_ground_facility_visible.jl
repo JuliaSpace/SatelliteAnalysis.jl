6b2f670f3ce5a12fc27e46c6a27361db9a83bf52
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2023-12-10 18:45:03 -0300

    :wrench: Improve variable naming

--- src/ground_facilities/is_ground_facility_visible.jl
+++ src/ground_facilities/is_ground_facility_visible.jl
@@ -7,13 +7,13 @@
 export is_ground_facility_visible
 
 """
-    is_ground_facility_visible(sat_r_e::AbstractVector, gs_r_e::AbstractVector, θ::Number) -> Bool
+    is_ground_facility_visible(sat_r_e::AbstractVector, gf_r_e::AbstractVector, θ::Number) -> Bool
 
 Check if the satellite with position vector `sat_r_e` (ECEF) is inside the visibility circle
-of a ground facility with position vector `gs_r_e` (ECEF) and a minimum elevation angle of
+of a ground facility with position vector `gf_r_e` (ECEF) and a minimum elevation angle of
 `θ` [rad].
 
-Notice that `sat_r_e` and `gs_r_e` must be represented in the same ECEF frame, and must have
+Notice that `sat_r_e` and `gf_r_e` must be represented in the same ECEF frame, and must have
 the same unit.
 
 # Returns
@@ -22,24 +22,24 @@ the same unit.
 """
 function is_ground_facility_visible(
     sat_r_e::AbstractVector,
-    gs_r_e::AbstractVector,
+    gf_r_e::AbstractVector,
     θ::Number
 )
     # Check if the satellite is within the visibility circle of the facility.
-    Δr_e = sat_r_e - gs_r_e
-    cos_β = dot(Δr_e / norm(Δr_e), gs_r_e / norm(gs_r_e))
+    Δr_e = sat_r_e - gf_r_e
+    cos_β = dot(Δr_e / norm(Δr_e), gf_r_e / norm(gf_r_e))
 
     return cos_β > cos(π / 2 - θ)
 end
 
 """
-    is_ground_facility_visible(sat_r_e::AbstractVector, gs_lat::Number, gs_lon::Number, gs_h::Number, θ::Number) -> Bool
+    is_ground_facility_visible(sat_r_e::AbstractVector, gf_lat::Number, gf_lon::Number, gf_h::Number, θ::Number) -> Bool
 
 Check if the satellite with position vector `sat_r_e` (ECEF) is inside the visibility circle
-of a ground facility with latitude `gs_lat` [rad], longitude `gs_lon` [rad], altitude `gs_h`
+of a ground facility with latitude `gf_lat` [rad], longitude `gf_lon` [rad], altitude `gf_h`
 (WGS-84), and a minimum elevation angle of `θ` [rad].
 
-Notice that `sat_r_e` and `gs_h` must have the same units.
+Notice that `sat_r_e` and `gf_h` must have the same units.
 
 # Returns
 
@@ -47,11 +47,11 @@ Notice that `sat_r_e` and `gs_h` must have the same units.
 """
 function is_ground_facility_visible(
     sat_r_e::AbstractVector,
-    gs_lat::Number,
-    gs_lon::Number,
-    gs_h::Number,
+    gf_lat::Number,
+    gf_lon::Number,
+    gf_h::Number,
     θ::Number
 )
-    gs_r_e = geodetic_to_ecef(gs_lat, gs_lon, gs_h)
-    return is_ground_facility_visible(sat_r_e, gs_r_e, θ)
+    gf_r_e = geodetic_to_ecef(gf_lat, gf_lon, gf_h)
+    return is_ground_facility_visible(sat_r_e, gf_r_e, θ)
 end
