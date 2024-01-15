655d16fddbe23a5d7748aa2fce45e364adc15968
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2024-01-14 23:06:06 -0300

    :rotating_light: Test extensions

--- test/plotting.jl
+++ test/plotting.jl
@@ -32,3 +32,29 @@ end
         plot_ground_track(1)
     )
 end
+
+@testset "Function plot_ground_track [EXT]" begin
+    using GeoMakie
+
+    jd₀ = date_to_jd(2021, 1, 1)
+    2.4592155e6
+
+    orb = KeplerianElements(
+        jd₀,
+        7130.982e3,
+        0.001111,
+        98.405 |> deg2rad,
+        ltdn_to_raan(10.5, jd₀),
+        π / 2,
+        0
+    )
+
+    orbp = Propagators.init(Val(:J2), orb)
+
+    gt = ground_track(orbp; track_types = :descending, duration = 5 * 86400);
+
+    fig, ax = plot_ground_track(gt; size = (2000, 1000))
+
+    @test fig isa Figure
+    @test ax isa Axis
+end
